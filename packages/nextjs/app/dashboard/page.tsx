928f5d74a8ceb2a6597b75b10b81b4664dd88c52
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-03-28 15:45:39 +0100

    modified dashboard

--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
+import React from "react";
 
 const Page = () => {
+  const [userType, setUserType] = React.useState("fan");
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <div className="flex py-8 justify-center gap-5">
+        <button className="btn btn-wide flex justify-center mt-2 btn-primary text-white text-xl" onClick={() => {setUserType("fan")}}>Fan</button>
+        <button className="btn btn-wide flex justify-center mt-2 btn-primary text-white text-xl" onClick={() => {setUserType("creator")}}>Creator</button>
+      </div>
+      <div className="px-12 pt-5">
+        {userType}
+      </div>
+    </>
+  );
+};
 
-export default Page
+export default Page;
