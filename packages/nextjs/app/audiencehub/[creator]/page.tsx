914bf8587a65e876f48631f643b85024267ee4b3
unknown <ariaworkshopnft@gmail.com>
2024-03-28 13:22:27 +0200

    progress

--- packages/nextjs/app/audiencehub/[creator]/page.tsx
+++ packages/nextjs/app/audiencehub/[creator]/page.tsx
@@ -1,20 +1,26 @@
 "use client";
 
 import { useEffect } from "react";
+import { useAccount } from "wagmi";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 
 export default function Page({ params }: { params: { creator: string } }) {
+
+    const {address} = useAccount();
+
+
   useEffect(() => {
     console.log(params);
   }, []);
 
 
   const { writeAsync, isLoading, isMining } = useScaffoldContractWrite({
-    contractName: "YourContract",
-    functionName: "setGreeting",
-    args: ["The value to set"],
+    contractName: "Sablier",
+    functionName: "createWithDurations",
+
+  args: [[address, "0x64336a17003cDCcde3cebEcff1CDEc2f9AeEdB7d", 10000000000000000000, "0x776b6fC2eD15D6Bb5Fc32e0c89DE68683118c62A", true, true, [0,3600], [0x0000000000000000000000000000000000000000,0]]],
     blockConfirmations: 1,
     onBlockConfirmation: txnReceipt => {
       console.log("Transaction blockHash", txnReceipt.blockHash);
@@ -30,8 +36,8 @@ export default function Page({ params }: { params: { creator: string } }) {
             <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
           </div>
         </div>
-        <div className="flex justify-center mt-5 font-bold">{params.creator}</div>
-        <button className="btn btn-wide flex justify-center mt-2 btn-primary text-white text-xl">Subscribe</button>
+        <div className="flex justify-center mt-5 font-bold">{params.creator} </div>
+        <button className="btn btn-wide flex justify-center mt-2 btn-primary text-white text-xl" onClick={() => writeAsync()}>Subscribe</button>
       </div>
     </>
   );
