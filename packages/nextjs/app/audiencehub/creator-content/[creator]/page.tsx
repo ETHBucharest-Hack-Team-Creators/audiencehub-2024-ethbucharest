d21dd7171eaf7119d35aa42ee1fbd3519c0e1875
ilge.ustun <ilge.ustun@gmail.com>
2024-03-29 14:14:19 +0200

    add content into /creator-content/[creator]/page

--- packages/nextjs/app/audiencehub/creator-content/[creator]/page.tsx
+++ packages/nextjs/app/audiencehub/creator-content/[creator]/page.tsx
@@ -1,28 +1,68 @@
-import React from 'react'
+"use client";
 
-const Page = ({ params }: { params: { creator: string } } ) => {
+import React, { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import ContentCard from "~~/components/ContentCard";
+import { useFB } from "~~/hooks/useFB";
+import { notification } from "~~/utils/scaffold-eth";
 
+export default function Page({ params }: { params: { creator: string } }) {
+  const [contentList, setContentList] = useState([]);
+  const [creatorData, setCreatorData] = useState({
+    name: "",
+  });
+  const [loading, setLoading] = useState(true);
 
-    //with creator address we query firebase
-    //we should get subscriptions/users/connected-user (useAccount)
+  const { address } = useAccount();
+  const { getCreatorContents, getCreatorData } = useFB();
 
-    //we should get subscribed: true
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async (address: string) => {
+      try {
+        const creator = await getCreatorData(address);
+        if (creator && creator.error) {
+          notification.error(creator.error);
+          throw new Error(creator.error);
+        }
+        setCreatorData(creator);
+        const content = await getCreatorContents(address);
+        console.log(content);
+        setContentList(content);
+      } catch (error) {
+        notification.error("Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    //
+    if (!address) return;
+    fetchData(address);
+  }, [address]);
 
-    // creator/subscripts/users 
-    // creator/content/
+  interface CardProps {
+    title: string;
+    description: string;
+    imgUrls: string[];
+    id: string;
+    // onClick: () => void;
+  }
 
-//    0x90 : {
-           
-//            streamId: 822, -> query metadata -> see status of the stream
-
-//     }
-
-
+  console.log("content list", contentList);
   return (
-    <div>   <div className="flex justify-center mt-5 font-bold">{params.creator} </div></div>
-  )
+    <div className="md-container max-w-screen-md md:mx-auto py-5 px-2">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1 className="text-2xl">{creatorData.name}</h1>
+          <ul>
+            {contentList.map((item: CardProps) => (
+              <ContentCard key={item.id} title={item.title} description={item.description} imgUrls={item.imgUrls} />
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
-
-export default Page
--- packages/nextjs/components/ContentCard.tsx
+++ packages/nextjs/components/ContentCard.tsx
@@ -1,23 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+
 // import Image from "next/image";
 
 interface CardProps {
   title: string;
   description: string;
-  imgUrl: string;
+  imgUrls: string;
   // onClick: () => void;
 }
 
-const ContentCard: React.FC<CardProps> = ({ title, description, imgUrl }) => {
+const ContentCard: React.FC<CardProps> = ({ title, description, imgUrls }) => {
   const handleClickCard = () => {
     console.log("card clicked");
   };
 
   return (
     <div className="content-card card card-side bg-base-100 shadow-xl my-7" onClick={handleClickCard}>
-      <figure className="sm:w-full md:w-1/2">
-        <img src={imgUrl} alt={title} style={{ height: "100%" }} />
+      <figure className="sm:w-full md:w-1/2 max-h-60 bg-transparent">
+        <div className="carousel carousel-center max-w-md p-4 space-x-4 rounded-box bg-transparent">
+          {imgUrls.map((item: string) => (
+            <div className="carousel-item" key={item}>
+              <img src={item} alt={title} style={{ height: "100%" }} className="rounded-box" />
+            </div>
+          ))}
+        </div>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
